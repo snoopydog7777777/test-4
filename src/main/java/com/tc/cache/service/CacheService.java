fa67b5039c4a7705a25ed6358ea7f895a428b893
snoopy <snoopydog7777777@gmail.com>
2022-05-12 18:28:22 +0900

    challenge

--- src/main/java/com/tc/cache/service/CacheService.java
+++ src/main/java/com/tc/cache/service/CacheService.java
@@ -1,5 +1,10 @@
 package com.tc.cache.service;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,15 +15,51 @@ import com.tc.cache.model.CacheData;
 @Component
 public class CacheService implements CacheApiDelegate {
 
+	@Value("${tc.cache.size}")
+	private int size;
+	
+	private ArrayList<CacheData> cacheDataList;
+	
+	public CacheService()
+	{
+		cacheDataList = new ArrayList();
+	}
+	
     @Override
     public ResponseEntity<Object> cacheIdGet(Integer id) {
         //TODO implement method
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    	CacheData currentData = null;
+    	int cacheSize = cacheDataList.size();
+    	for(int i = 0; i < cacheSize; i++)
+    	{
+    		CacheData c = cacheDataList.get(i); 
+    		if(c.getId() == id)
+    		{
+    			currentData = c;
+    			cacheDataList.remove(i);
+    			cacheDataList.add(currentData);
+    			break;
+    		}
+    	}
+    	
+    	if(currentData == null)
+    		return new ResponseEntity<>("Cache not exist", HttpStatus.OK);
+    	return new ResponseEntity<>(currentData, HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> cachePut(CacheData cacheData) {
         //TODO implement method
-        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    	int cacheDataLength = cacheDataList.size();
+    	if(cacheDataLength >= size)
+    	{
+    		cacheDataList.remove(0);
+    	}
+    	
+    	cacheDataList.add(cacheData);
+
+    	System.out.println("cacheDataList:" + cacheDataList);
+    	
+    	return new ResponseEntity<>(HttpStatus.OK);
     }
 }
